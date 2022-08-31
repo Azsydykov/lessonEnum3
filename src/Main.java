44184150fb161b3f9fcaa7cd95d3cc5ca086a98f
Azsydykov <57948994+Azsydykov@users.noreply.github.com>
2022-08-31 19:12:54 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import enums.Elements;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (Elements elements: Elements.values()){
+            System.out.println(elements);
+        }
+
+
+    }
+}
---
+++ src/enums/Elements.java
@@ -0,0 +1,56 @@
+package enums;
+
+public enum Elements {
+    HELIUM("Гелий", "He", 2),
+    BORUM("Бор","B",5),
+    NEON("Неон", "Ne", 10),
+    LITHIUM("Литий", "Li", 3),
+    NATRIUM("Натрий", "Na",11),
+    CHLORUM("Хлор", "Cl",17);
+
+
+    private String name;
+    private String symbol;
+    private int atomNumber;
+
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAtomNumber() {
+        return atomNumber;
+    }
+
+    public void setAtomNumber(int atomNumber) {
+        this.atomNumber = atomNumber;
+    }
+
+    Elements(String name,String symbol, int atomNumber) {
+        this.name = name;
+        this.symbol = symbol;
+        this.atomNumber = atomNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Elements[" +
+                "name='" + name + '\'' +
+                ", symbol='" + symbol + '\'' +
+                ", atomNumber=" + atomNumber +
+                ']';
+    }
+}
